4846e093f15efe103dc064aa91b4e6bf7166bcd8
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2025-03-25 00:52:53 -0400

    more sensible testing code

--- src/test.py
+++ src/test.py
@@ -1,6 +1,8 @@
-from datasets import Dataset
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from datasets import load_dataset
+from transformers import AutoModelForCausalLM, AutoTokenizer, get_scheduler
 from accelerate import Accelerator
+from torch.utils.data import DataLoader
+import bitsandbytes as bnb
 
 from aurl import GRPOTrainer
 
@@ -11,6 +13,17 @@ def gsm8k_reward(output: str, answer: str):
         return 0.0
 
 if __name__ == "__main__":
+    epochs = 1
+    batch_size = 1
+    num_warmup_steps = 1
+    
+    adam_betas = (0.9, 0.98)
+    adam_weight_decay = 0.01
+    initial_lr = 3e-6
+    
+    clip_grad_norm = 0.01
+    
+    
     accelerator = Accelerator(
         gradient_accumulation_steps=1,
         mixed_precision="bf16",
@@ -21,7 +34,11 @@ if __name__ == "__main__":
     ref = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-0.5B")
     tok = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-0.5B")
     
-    dataset = Dataset.from_json("data/gsm8k.jsonl")
+    dataset = load_dataset("json", data_files="data/gsm8k.jsonl")
+    
+    train_dataloader = DataLoader(
+        dataset, batch_size=batch_size, shuffle=True
+    )
     
     trainer = GRPOTrainer(
         accelerator,
@@ -31,8 +48,36 @@ if __name__ == "__main__":
         [gsm8k_reward]
     )
     
-    for i in range(10):
-        rollouts = trainer.generate_rollouts([dataset[i]])
-        loss = trainer.compute_loss(rollouts)
+    optimizer = bnb.optim.Adam8bit(
+        model.parameters(),
+        lr=initial_lr,
+        betas=(adam_betas[0], adam_betas[1]),
+        weight_decay=adam_weight_decay,
+    )
     
-    trainer.train()
+    lr_scheduler = get_scheduler(
+        name="linear",
+        optimizer=optimizer,
+        num_warmup_steps=num_warmup_steps,
+        num_training_steps=len(train_dataloader) * epochs,
+    )
+    
+    model.train()
+    for epoch in range(epochs):
+        for step, batch in enumerate(dataset):
+            model.eval()
+            rollouts = trainer.generate_rollouts()
+            model.train()
+            
+            with accelerator.accumulate(model):
+                loss = trainer.compute_loss()
+                
+                accelerator.backward(loss)
+                accelerator.clip_grad_norm_(
+                    model.parameters(), clip_grad_norm
+                )
+                
+                optimizer.step()
+                lr_scheduler.step()
+                
+                optimizer.zero_grad()
