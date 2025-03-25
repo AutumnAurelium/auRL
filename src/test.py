8a54961f3e0adc8c03dea6da7a0462fbb5f739c7
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2025-03-25 01:37:12 -0400

    answers

--- src/test.py
+++ src/test.py
@@ -10,7 +10,7 @@ from aurl import GRPOTrainer
 def gsm8k_reward(prompts: list[str], completions: list[str], answer: str):
     rewards = []
     for completion in completions:
-        if answer in completion:
+        if answer[0] in completion:
             rewards.append(1.0)
         else:
             rewards.append(0.0)
