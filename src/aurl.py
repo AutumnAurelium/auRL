d4d5a5b5ca082d0e84595f4212ab526643467bb5
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2025-03-25 02:09:50 -0400

    mean_grouped_rewards

--- src/aurl.py
+++ src/aurl.py
@@ -276,6 +276,9 @@ class GRPOTrainer:
 
         # Compute group-wise rewards
         mean_grouped_rewards = rewards.view(-1, self.num_generations).mean(dim=1)
+        
+        print(mean_grouped_rewards)
+        
         std_grouped_rewards = rewards.view(-1, self.num_generations).std(dim=1)
 
         # Normalize the rewards to compute the advantages
@@ -295,58 +298,6 @@ class GRPOTrainer:
             (self.accelerator.process_index + 1) * len(prompts),
         )
         advantages = advantages[process_slice]
-
-        # Log the metrics
-        # mode = "eval" if self.control.should_evaluate else "train"
-
-        # if mode == "train":
-        #     self._total_train_tokens += (
-        #         self.accelerator.gather_for_metrics(attention_mask.sum()).sum().item()
-        #     )
-        # self._metrics[mode]["num_tokens"] = [self._total_train_tokens]
-
-        # completion_length = self.accelerator.gather_for_metrics(completion_mask.sum(1)).float().mean().item()
-        # self._metrics[mode]["completion_length"].append(completion_length)
-
-        # Calculate mean reward per function, but only for samples where the function was applied
-        # for i, reward_func in enumerate(self.reward_funcs):
-            # if isinstance(
-                # reward_func, nn.Module
-            # ):  # Module instead of PretrainedModel for compat with compiled models
-                # reward_func_name = reward_func.config._name_or_path.split("/")[-1]
-            # else:
-                # reward_func_name = reward_func.__name__
-            # Only calculate mean for samples where this reward function was applied (non-NaN values)
-            # mean_rewards = torch.nanmean(rewards_per_func[:, i]).item()
-            # self._metrics[mode][f"rewards/{reward_func_name}"].append(mean_rewards)
-        # self._metrics[mode]["reward"].append(rewards.mean().item())
-        # self._metrics[mode]["reward_std"].append(std_grouped_rewards.mean().item())
-
-        # if self.log_completions and self.state.global_step % self.args.logging_steps == 0:
-        #     prompts_to_log = gather_object(prompts_text)
-        #     completions_to_log = gather_object(completions_text)
-        #     rewards_to_log = rewards.tolist()
-
-        #     if self.accelerator.is_main_process:
-        #         if is_rich_available():
-        #             print_prompt_completions_sample(
-        #                 prompts_to_log,
-        #                 completions_to_log,
-        #                 rewards_to_log,
-        #                 self.state.global_step,
-        #             )
-        #         if self.args.report_to and "wandb" in self.args.report_to and wandb.run is not None:
-        #             import pandas as pd
-
-        #             # For logging
-        #             table = {
-        #                 "step": [str(self.state.global_step)] * len(rewards),
-        #                 "prompt": prompts_to_log,
-        #                 "completion": completions_to_log,
-        #                 "reward": rewards.tolist(),
-        #             }
-        #             df = pd.DataFrame(table)
-        #             wandb.log({"completions": wandb.Table(dataframe=df)})
 
         return {
             "prompt_ids": prompt_ids,
