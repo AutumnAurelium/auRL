29b2ed12510f527db3376df9df824a2a1057cef1
AutumnAurelium <88015631+AutumnAurelium@users.noreply.github.com>
2025-03-25 14:17:23 -0400

    retry table

--- src/test.py
+++ src/test.py
@@ -122,7 +122,6 @@ if __name__ == "__main__":
                         for completion in completions:
                             completions_table.add_data(step, batch["prompt"][0], completion)
                             
-                        wandb.log({"completions": completions_table})
                         wandb.log(metrics)
                     
                     accelerator.backward(loss)
